54c8804a3a58e4c11d9cb049f5412f01a6336eaf
kajo2001 <kajalbhalerao75@gmail.com>
2026-06-30 12:18:01 +0530

    Added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 var mysql=require("mysql2");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 var con= mysql.createConnection({
   host:"mydatabase.cjmamy44e1z1.eu-north-1.rds.amazonaws.com",
   user:"admin",
@@ -23,8 +25,25 @@ app.get("/api/student",(req,res)=>{
 app.get('/', (req, res) => {
     // res.send('Hello Express!');
     res.sendFile(__dirname + '/index.html');
+
+    
 });
 
+app.get('/student', (req, res) => {
+  con.connect(function(err){
+    if(err) throw err;
+    con.query("select * from Students",(err,result)=>{
+      if(err) throw err;
+     res.render('index', { 
+            pageTitle: 'User Directory (Rendered on Server)', 
+            users: result 
+        });
+
+    })
+  })
+
+    
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
